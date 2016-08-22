5439b3291d11da0398c9e5f4d468186c4ddd1dd9
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2016-08-22 17:38:45 +0200

    Init

---
+++ README.md
@@ -0,0 +1,63 @@
+# Newsletter2Go Provider for OAuth 2.0 Client
+
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]]()
+[![Dependency Status][ico-dependencies]][link-dependencies]
+
+This package provides Newsletter2Go OAuth 2.0 support for the PHP League's [OAuth 2.0 Client](https://github.com/thephpleague/oauth2-client).
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require richardhj/oauth2-newsletter2go
+```
+
+## Usage
+
+Use the auth key from your Newsletter2Go account to initiate the provider.
+
+```php
+$provider = new Newsletter2Go\OAuth2\Client\Provider\Newsletter2Go([
+    'authKey' => $authKey,
+]);
+```
+
+Then use your login credentials to fetch an AccessToken instance.
+
+```php
+$accessToken = $provider->getAccessToken(
+    'https://nl2go.com/jwt',
+    [
+        'username' => $username,
+        'password' => $password,
+    ]
+);
+```
+
+### Refreshing a token
+
+Initiate the provider as desribed before. Then:
+
+```php
+$accessToken = $provider->getAccessToken(
+    'https://nl2go.com/jwt_refresh',
+    [
+        'refresh_token' => $accessToken->getRefreshToken()
+    ]
+);
+```
+
+Visit [the official API documentation](https://docs.newsletter2go.com/#/Authorization) for reference.
+
+## License
+
+The  GNU Lesser General Public License (LGPL).
+
+[ico-version]: https://img.shields.io/packagist/v/richardhj/oauth2-newsletter2go.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-LGPL-brightgreen.svg?style=flat-square
+[ico-dependencies]: https://www.versioneye.com/php/richardhj:oauth2-newsletter2go/badge.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/richardhj/oauth2-newsletter2go
+[link-dependencies]: https://www.versioneye.com/php/richardhj:oauth2-newsletter2go
---
+++ src/Newsletter2Go/OAuth2/Client/Provider/Newsletter2Go.php
@@ -0,0 +1,241 @@
+<?php
+/**
+ * Newsletter2Go API integration
+ *
+ * @copyright Copyright (c) 2016 Richard Henkenjohann
+ * @license   LGPL-3.0+
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ */
+
+namespace Newsletter2Go\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\GenericResourceOwner;
+use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
+
+
+/**
+ * Class Newsletter2Go
+ * @package Newsletter2Go\OAuth2\Client\Provider
+ */
+class Newsletter2Go extends AbstractProvider
+{
+
+    /**
+     * The endpoint base url
+     *
+     * @var string
+     */
+    protected static $endpoint = 'https://api.newsletter2go.com';
+
+
+    /**
+     * Map the grant types used per default with the grant type names used by Newsletter2Go
+     *
+     * @var array
+     */
+    protected static $grantMappings = [
+        'password'      => 'https://nl2go.com/jwt',
+        'refresh_token' => 'https://nl2go.com/jwt_refresh',
+    ];
+
+
+    /**
+     * The user's auth key
+     *
+     * @var string
+     */
+    protected $authKey;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $options = [], array $collaborators = [])
+    {
+        $this->assertRequiredOptions($options);
+
+        $possible = $this->getConfigurableOptions();
+        $configured = array_intersect_key($options, array_flip($possible));
+
+        foreach ($configured as $key => $value) {
+            $this->$key = $value;
+        }
+
+        // Remove all options that are only used locally
+        $options = array_diff_key($options, $configured);
+
+        parent::__construct($options, $collaborators);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        throw new \BadFunctionCallException(
+            ' is not supported by the Newsletter2Go OAuth implementation'
+        );
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return static::$endpoint.'/oauth/v2/token';
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return static::$endpoint.'/users?_limit=-1&_expand=true';
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+
+    /**
+     * Add authorization header
+     *
+     * {@inheritdoc}
+     */
+    protected function getAccessTokenOptions(array $params)
+    {
+        $options = parent::getAccessTokenOptions($params);
+
+        $options = array_merge_recursive(
+            $options,
+            [
+                'headers' => [
+                    'Authorization' => 'Basic '.base64_encode($this->authKey),
+                ],
+            ]
+        );
+
+        return $options;
+    }
+
+
+    /**
+     * Alter the grant_type as Newsletter2Go has it owns
+     *
+     * {@inheritdoc}
+     */
+    public function getAccessToken($grant, array $options = [])
+    {
+        if (false !== ($grantDefault = array_search($grant, static::$grantMappings))) {
+
+            $options = array_merge(
+                $options,
+                [
+                    'grant_type' => $grant,
+                ]
+            );
+
+            $grant = $grantDefault;
+        }
+
+        return parent::getAccessToken($grant, $options);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (!empty($data['error'])) {
+            throw new IdentityProviderException($data['error'], $response->getStatusCode(), $data);
+        }
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        $users = $response['value'];
+        $users = array_filter(
+            $users,
+            function ($v) use ($token) {
+                return $token->getValues()['account_id'] === $v['account_id'];
+            }
+        );
+
+        return new GenericResourceOwner(reset($users), 'id');
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthorizationHeaders($token = null)
+    {
+        return [
+            'Authorization' => 'Bearer '.(($token instanceof AccessToken) ? $token->getToken() : $token),
+        ];
+    }
+
+
+    /**
+     * Returns all options that can be configured
+     *
+     * @return array
+     */
+    protected function getConfigurableOptions()
+    {
+        return array_merge(
+            $this->getRequiredOptions(),
+            []
+        );
+    }
+
+
+    /**
+     * Returns all options that are required
+     *
+     * @return array
+     */
+    protected function getRequiredOptions()
+    {
+        return [
+            'authKey',
+        ];
+    }
+
+
+    /**
+     * Verifies that all required options have been passed
+     *
+     * @param  array $options
+     *
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    protected function assertRequiredOptions(array $options)
+    {
+        $missing = array_diff_key(array_flip($this->getRequiredOptions()), $options);
+
+        if (!empty($missing)) {
+            throw new \InvalidArgumentException(
+                'Required options not defined: '.implode(', ', array_keys($missing))
+            );
+        }
+    }
+}
